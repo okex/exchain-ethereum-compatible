e23cb3ae8aec834fcbf3ee9d72fd8ac5f5f34a26
Green <gl12138@live.com>
2021-08-27 15:30:09 +0800

    add hash function

---
+++ go.mod
@@ -0,0 +1,8 @@
+module exchain_ethereum_compatible
+
+go 1.14
+
+require (
+	github.com/ethereum/go-ethereum v1.10.4
+	github.com/tendermint/go-amino v0.15.1
+)
---
+++ utils/hash.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"crypto/sha256"
+	"errors"
+	"fmt"
+
+	"exchain_ethereum_compatible/internal"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+func Hash(signtx types.Transaction) (common.Hash, error) {
+	if signtx.Type() != types.LegacyTxType {
+		return common.Hash{}, errors.New("only supported eip-155 legacy transaction")
+	}
+
+	v, r, s := signtx.RawSignatureValues()
+	msg := internal.NewMsgEthereumTx(
+		signtx.Nonce(),
+		signtx.GasPrice(),
+		signtx.Gas(),
+		signtx.To(),
+		signtx.Value(),
+		signtx.Data(),
+		v,
+		r,
+		s,
+	)
+
+	bins, err := marshal(msg)
+	if err != nil {
+		return common.Hash{}, errors.New(fmt.Sprintf("failed to marshal msg: %v", err))
+	}
+
+	hash := sha256.Sum256(bins)
+	return common.BytesToHash(hash[:]), nil
+}
+
+func marshal(msg internal.MsgEthereumTx) ([]byte, error) {
+	cdc := internal.GetModuleCdc()
+	return cdc.MarshalBinaryLengthPrefixed(msg)
+}

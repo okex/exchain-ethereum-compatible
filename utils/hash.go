f804be37643ab7a138d026a2305551e9fde4afae
Zhong Qiu <36867992+zhongqiuwood@users.noreply.github.com>
2022-01-05 19:21:22 +0800

    ethtxhash (#1)

--- utils/hash.go
+++ utils/hash.go
@@ -4,13 +4,24 @@ import (
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	"golang.org/x/crypto/sha3"
+	"hash"
+	"sync"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/okex/exchain-ethereum-compatible/internal"
 )
 
+func LegacyHash(signtx *types.Transaction) (common.Hash, error) {
+	return txhash(signtx, cmHash)
+}
+
 func Hash(signtx *types.Transaction) (common.Hash, error) {
+	return txhash(signtx, ethHash)
+}
+
+func txhash(signtx *types.Transaction, hash func(data []byte)common.Hash) (common.Hash, error) {
 	if signtx.Type() != types.LegacyTxType {
 		return common.Hash{}, errors.New("only supported eip-155 legacy transaction")
 	}
@@ -33,9 +44,38 @@ func Hash(signtx *types.Transaction) (common.Hash, error) {
 		return common.Hash{}, errors.New(fmt.Sprintf("failed to marshal msg: %v", err))
 	}
 
-	hash := sha256.Sum256(bins)
-	return common.BytesToHash(hash[:]), nil
+	return hash(bins), nil
 }
+
+func cmHash(data []byte) common.Hash {
+	hash := sha256.Sum256(data)
+	return common.BytesToHash(hash[:])
+}
+
+func ethHash(data []byte) common.Hash {
+	hash := sum(data)
+	return common.BytesToHash(hash[:])
+}
+
+var keccakPool = sync.Pool{
+	// NewLegacyKeccak256 uses non-standard padding
+	// and is incompatible with sha3.Sum256
+	New: func() interface{} { return sha3.NewLegacyKeccak256() },
+}
+
+// Sum returns the non-standard Keccak256 of the bz.
+func sum(bz []byte) []byte {
+	sha := keccakPool.Get().(hash.Hash)
+	defer func() {
+		// better to reset before putting it to the pool
+		sha.Reset()
+		keccakPool.Put(sha)
+	}()
+	sha.Reset()
+	sha.Write(bz)
+	return sha.Sum(nil)
+}
+
 
 func marshal(msg internal.MsgEthereumTx) ([]byte, error) {
 	cdc := internal.GetModuleCdc()
